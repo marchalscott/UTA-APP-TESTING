c6377a0689d0a696e0923bdcc54bf861bc340b66
marchalscott <marchalscott@gmail.com>
2019-04-05 16:48:02 -0600

    First Commit for this project upload

---
+++ app/components/Login.vue
@@ -0,0 +1,17 @@
+<template>
+    <page>
+        <ActionBar title="Login" class="action-bar" />
+        <StackLayout>
+        <Label text="login here"></Label>
+        </StackLayout>
+    </page>
+</template>
+
+<script>
+export default {
+    methods: {
+
+    }
+}
+</script>
+
