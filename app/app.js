3ed4d15587a0af699e16eca0966c0aa31e927eb3
marchalscott <marchalscott@gmail.com>
2019-04-08 14:51:02 -0600

    testing layouts

--- app/app.js
+++ app/app.js
@@ -2,6 +2,9 @@ import Vue from "nativescript-vue";
 
 import Home from "./components/Home";
 
+import { registerElement } from "nativescript-angular";
+Vue.registerElement("Gradient", () => require("nativescript-gradient").Gradient);
+
 new Vue({
 
     template: `
--- app/components/Login.vue
+++ app/components/Login.vue
@@ -1,9 +1,10 @@
 <template>
     <page>
         <ActionBar title="Login" class="action-bar" />
-        <StackLayout>
-        <Label text="login here"></Label>
-        </StackLayout>
+<Gradient direction="to right" colors="#FF0077, red, #FF00FF">
+  <Label class="p-20 c-white" horizontalAlignment="center" text="My gradients are the best." textWrap="true"></Label>
+  <Label class="p-10 c-white" horizontalAlignment="center" text="It's true." textWrap="true"></Label>
+</Gradient>
     </page>
 </template>
 
